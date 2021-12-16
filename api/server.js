b0fd5940fc9a2ad476f77669a8c4877e068f6912
LRose7 <lindseyurquhart@tutanota.com>
2021-12-16 17:00:00 -0500

    Initial Commit

--- api/server.js
+++ api/server.js
@@ -1,1 +1,32 @@
+if (process.env.NODE_ENV = 'production') {
+    require('dotenv').config();
+}
+const express = require('express');
+const app = express(); // set up express app
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) throw err;
+    console.log('MONGODB connection established');
+});
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('common'));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
